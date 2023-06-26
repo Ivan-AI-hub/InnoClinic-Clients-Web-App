e69799a15e21a0c0ca96d636a80c0d610e727107
Ivan Andreevets <ivan_grozdny@mail.ru>
2023-06-26 10:24:14 +0300

    initial commit

---
+++ ClientsWebApp/ClientsWebApp.Blazor/Program.cs
@@ -0,0 +1,11 @@
+using ClientsWebApp.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
