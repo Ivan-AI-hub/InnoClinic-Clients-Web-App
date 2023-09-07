c1cd3f3f21bf4dbc57b6bfb5429d3c898bd7d8fb
Ivan Andreevets <ivan_grozdny@mail.ru>
2023-09-07 23:00:33 +0200

    Impliment Localization

--- ClientsWebApp/ClientsWebApp.Blazor/Components/CancellableComponent.cs
+++ ClientsWebApp/ClientsWebApp.Blazor/Components/CancellableComponent.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using ClientsWebApp.Blazor.AppLocalization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 namespace ClientsWebApp.Blazor.Components
 {
     public class CancellableComponent : ComponentBase, IDisposable
     {
+        [Inject] protected IStringLocalizer<Localization> _localizer { get; set; }
+
         protected CancellationTokenSource _cts = new();
         public void Dispose()
         {
